12631d5880a22ab125c90d03d716265a35e06ccf
Ben Mills <fuzzyllama@gmail.com>
2021-09-15 14:22:37 -0700

    refactors app.js to use for loops and switch statements

--- js/app.js
+++ js/app.js
@@ -1,25 +1,24 @@
 'use strict';
 
 // Questions
-let question01 =
-  'First Question: Do you think I\'ve ever travelled outside of the country?';
-let question02 =
-  'Second Question: Sticking with the traveling theme do you think it\'s true that I have visited all 50 states in the US?';
-let question03 = 'Third Question: Do I play video games?';
-let question04 = 'Fourth Question: Have I ever prepared a bowl of ramen?';
-let question05 = 'Fifth and Final Question: Do I do much DIY?';
+let questions = [
+  'First Question: Do you think I\'ve ever travelled outside of the country?',
+  'Second Question: Sticking with the traveling theme do you think it\'s true that I have visited all 50 states in the US?',
+  'Third Question: Do I play video games?',
+  'Fourth Question: Have I ever prepared a bowl of ramen?',
+  'Fifth and Final Question: Do I do much DIY?',
+];
 
 // Responses
-let response01 =
-  'I\'ve travelled to Japan many times and I studied Japanese language in college, but my language skills are mediocre at best. My two kiddos have dual citizinship and speak Japanese fluently though.';
-let response02 =
-  'I\'ve only traveled to 28 states but I did take a Greyhound bus from Seattle to NYC when I was 18.';
-let response03 =
-  'I got my NES when I was about 5 and haven\'t stopped since. Lately I\'ve been getting back into retro games on my MiSTer FPGA.';
-let response04 =
-  'I found a really great recipe and make it from scratch (noodles, chashu, broth and all) about once a month.';
-let response05 =
-  'I\'ve replaced most of the windows in our house; installed new cabnetry in the kitchen; installed hardwood floors; and built a deck out back.';
+let responses = [
+  'I\'ve travelled to Japan many times and I studied Japanese language in college, but my language skills are mediocre at best. My two kiddos have dual citizinship and speak Japanese fluently though.',
+  'I\'ve only traveled to 28 states but I did take a Greyhound bus from Seattle to NYC when I was 18.',
+  'I got my NES when I was about 5 and haven\'t stopped since. Lately I\'ve been getting back into retro games on my MiSTer FPGA.',
+  'I found a really great recipe and make it from scratch (noodles, chashu, broth and all) about once a month.',
+  'I\'ve replaced most of the windows in our house, installed new cabnetry in the kitchen, installed hardwood floors, and built a deck out back.',
+];
+
+let answers = [true, false, true, true, true];
 
 // Begin Popup City
 let visiterName = prompt(
@@ -30,71 +29,49 @@ let introMessage =
   visiterName +
   ', please participate in this breif 5 question guessing game. Answers can be in yes/no or y/s form.';
 
-// First Question
-let answer01 = prompt(introMessage + '\n\n' + question01);
-if (answer01.toLowerCase() === 'y' || answer01.toLowerCase() === 'yes') {
-  // console.log("Correct! " + response01 + ".");
-  alert('Correct! ' + response01 + '.');
-} else if (answer01.toLowerCase() === 'n' || answer01.toLowerCase() === 'no') {
-  // console.log("Incorrect, actually " + response01 + ".");
-  alert('Incorrect, actually ' + response01 + '.');
-} else {
-  // console.log("Sorry I didn't get that." + answer01);
-  alert('Sorry I didn\'t get that.' + answer01);
+for (let i = 0; i < questions.length; i++) {
+  let answer = null;
+  if (i === 0) {
+    answer = prompt(introMessage + '\n\n' + questions[i]);
+  } else {
+    answer = prompt(questions[i]);
+  }
+  if (answers[i] === true) {
+    switch (answer.toLowerCase()) {
+    case 'y':
+      alert('Correct! ' + responses[i] + '.');
+      break;
+    case 'n':
+      alert('Incorrect, actually ' + responses[i] + '.');
+      break;
+    case 'yes':
+      alert('Correct! ' + responses[i] + '.');
+      break;
+    case 'no':
+      alert('Incorrect, actually ' + responses[i] + '.');
+      break;
+    default:
+      console.log('Sorry I didn\'t catch that.');
+    }
+  } else if (answers[i] === false) {
+    switch (answer.toLowerCase()) {
+    case 'y':
+      alert('Incorrect, actually ' + responses[i] + '.');
+      break;
+    case 'n':
+      alert('Correct! ' + responses[i] + '.');
+      break;
+    case 'yes':
+      alert('Incorrect, actually ' + responses[i] + '.');
+      break;
+    case 'no':
+      alert('Correct! ' + responses[i] + '.');
+      break;
+    default:
+      console.log('Sorry I didn\'t catch that.');
+    }
+  }
 }
-
-// Second Question
-let answer02 = prompt(question02);
-if (answer02.toLowerCase() === 'n' || answer02.toLowerCase() === 'no') {
-  // console.log("Correct! " + response02);
-  alert('Correct! ' + response02);
-} else if (answer02.toLowerCase() === 'y' || answer02.toLowerCase() === 'yes') {
-  // console.log("Incorrect, actually " + response02);
-  alert('Incorrect, actually ' + response02);
-} else {
-  // console.log("Sorry I didn't get that.");
-  alert('Sorry I didn\'t get that.');
-}
-
-// Third Question
-let answer03 = prompt(question03);
-if (answer03.toLowerCase() === 'y' || answer03.toLowerCase() === 'yes') {
-  // console.log("Correct! " + response03);
-  alert('Correct! ' + response03);
-} else if (answer03.toLowerCase() === 'n' || answer03.toLowerCase() === 'no') {
-  // console.log("Incorrect, actually " + response03);
-  alert('Incorrect, actually ' + response03);
-} else {
-  // console.log("Sorry I didn't get that.");
-  alert('Sorry I didn\'t get that.');
-}
-
-// Fourth Question
-let answer04 = prompt(question04);
-if (answer04.toLowerCase() === 'y' || answer04.toLowerCase() === 'yes') {
-  // console.log("Correct! " + response04);
-  alert('Correct! ' + response04);
-} else if (answer04.toLowerCase() === 'n' || answer04.toLowerCase() === 'no') {
-  // console.log("Incorrect, actually " + response04);
-  alert('Incorrect, actually ' + response04);
-} else {
-  // console.log("Sorry I didn't get that.");
-  alert('Sorry I didn\'t get that.');
-}
-
-// Fifth Question
-let answer05 = prompt(question05);
-if (answer05.toLowerCase() === 'y' || answer05.toLowerCase() === 'yes') {
-  // console.log("Correct! " + response05);
-  alert('Correct! ' + response05);
-} else if (answer05.toLowerCase() === 'n' || answer05.toLowerCase() === 'no') {
-  // console.log("Incorrect, actually " + response05);
-  alert('Incorrect, actually ' + response05);
-} else {
-  // console.log("Sorry I didn't get that.");
-  alert('Sorry I didn\'t get that.');
-}
-
 //cowsay goodbye alert
 alert(
   ' _________________________________________\n/ Thank you for playing my guessing game. \\\n\\ have a good day ' +
